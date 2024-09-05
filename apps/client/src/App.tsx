a81819cd4c5bb149da48790927edcd68e4886ce7
Adrian_Diaz <adriandiaz15196@gmail.com>
2024-09-05 16:41:46 -0600

    Falta verificar del porque no funciona juntos pero si levanta el servicio y tambien documentacion de global-packages

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { UserPayload } from 'global-interfaces'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  
+  const [count, setCount] = useState(0)
+  const user: UserPayload = {email: '',id:count, name:""};
   return (
     <>
       <div>
@@ -16,7 +19,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React
+        <hr />
+    {JSON.stringify({user})}
+
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
